ee35c696b23ddaabf9e5c58af734dc3dfcf829a9
Basil Baby <basilbaby112@gmail.com>
2024-05-30 00:05:52 +0530

    changes

--- resources/views/marks/form.blade.php
+++ resources/views/marks/form.blade.php
@@ -4,26 +4,27 @@
 <div class="container">
     <h3>{{ isset($mark) ? 'Edit' : 'Add' }} Marks</h3>
 
-    <form action="{{ isset($mark) ? route('marks.update', $mark) : route('marks.store') }}" method="POST">
+    <form class="row g-3" action="{{ isset($mark) ? route('marks.update', $mark) : route('marks.store') }}" method="POST">
         @csrf
         @if(isset($mark))
             @method('PUT')
         @endif
 
-        <div>
-            <label>Student</label>
-            <select name="student_id">
+        <div class="col-md-6">
+            <label class="form-label">Student</label>
+            <select class="form-select" name="student_id">
+                <option value="">Select Student</option>
                 @foreach($students as $student)
                     <option value="{{ $student->id }}" {{ isset($mark) && $mark->student_id == $student->id ? 'selected' : '' }}>{{ $student->name }}</option>
                 @endforeach
             </select>
         </div>
-        <div>
-            <label>Term</label>
+        <div class="col-md-6">
+            <label class="form-label">Term</label>
             @php
                 $terms =['one','two','three'];
             @endphp
-            <select name="term" id="">
+            <select class="form-select" name="term" id="">
                 <option value="">select</option>
                 @foreach ($terms as $term)
                 <option value="{{$term}}" {{isset($mark)&& $mark->term == $term ? 'selected' : ''}}>{{$term}}</option>
@@ -31,19 +32,19 @@
             </select>
             {{-- <input type="text" name="term" value="{{ isset($mark) ? $mark->term : '' }}"> --}}
         </div>
-        <div>
-            <label>Maths</label>
-            <input type="number" name="maths" value="{{ isset($mark) ? $mark->maths : ''}}">
-            <label>Science</label>
-            <input type="number" name="science" value="{{ isset($mark) ? $mark->science : ''}}">
-            <label>History</label>
-            <input type="number" name="history" value="{{ isset($mark) ? $mark->history : ''}}">
-            
+        <div class="col-md-4">
+            <label class="form-label">Maths</label>
+            <input class="form-control" type="number" name="maths" value="{{ isset($mark) ? $mark->maths : ''}}"> 
         </div>
-        <div>
-            
+        <div class="col-md-4">
+            <label class="form-label">Science</label>
+            <input class="form-control" type="number" name="science" value="{{ isset($mark) ? $mark->science : ''}}">
         </div>
-        <button type="submit">Submit</button>
+        <div class="col-md-4">
+            <label class="form-label">History</label>
+            <input class="form-control" type="number" name="history" value="{{ isset($mark) ? $mark->history : ''}}">
+        </div>
+        <button class="btn btn-primary" type="submit">Submit</button>
     </form>
 </div>
 @endsection
--- resources/views/students/form.blade.php
+++ resources/views/students/form.blade.php
@@ -4,36 +4,39 @@
 <div class="container">
     <h3>{{ isset($student) ? 'Edit' : 'Add' }} Student</h3>
 
-    <form action="{{ isset($student) ? route('students.update', $student) : route('students.store') }}" method="POST">
+    <form class="row g-3" action="{{ isset($student) ? route('students.update', $student) : route('students.store') }}" method="POST">
         @csrf
         @if(isset($student))
             @method('PUT')
         @endif
 
-        <div>
-            <label>Name</label>
-            <input type="text" name="name" value="{{ isset($student) ? $student->name : '' }}">
+        <div class="col-md-6">
+            <label class="form-label">Name</label>
+            <input class="form-control" type="text" name="name" value="{{ isset($student) ? $student->name : '' }}">
         </div>
-        <div>
-            <label>Age</label>
-            <input type="number" name="age" value="{{ isset($student) ? $student->age : '' }}">
+        <div class="col-md-6">
+            <label class="form-label">Age</label>
+            <input class="form-control" type="number" name="age" value="{{ isset($student) ? $student->age : '' }}">
         </div>
-        <div>
-            <label>Gender</label>
-            <select name="gender">
+        <div class="col-md-6">
+            <label class="form-label">Gender</label>
+            <select class="form-select" name="gender">
                 <option value="male" {{ isset($student) && $student->gender == 'male' ? 'selected' : '' }}>Male</option>
                 <option value="female" {{ isset($student) && $student->gender == 'female' ? 'selected' : '' }}>Female</option>
             </select>
         </div>
-        <div>
-            <label>Teacher</label>
-            <select name="teacher_id">
+        <div class="col-md-6">
+            <label class="form-label">Teacher</label>
+            <select class="form-select" name="teacher_id">
+                <option value="">Select Teacher</option>
                 @foreach($teachers as $teacher)
                     <option value="{{ $teacher->id }}" {{ isset($student) && $student->teacher_id == $teacher->id ? 'selected' : '' }}>{{ $teacher->name }}</option>
                 @endforeach
             </select>
         </div>
-        <button type="submit">Submit</button>
+        <div class="col-md-6">
+        <button class="btn btn-primary" type="submit">Submit</button>
+        </div>
     </form>
 </div>
 @endsection
